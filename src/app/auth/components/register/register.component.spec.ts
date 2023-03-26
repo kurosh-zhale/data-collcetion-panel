1822e942052b405f367682a2e1aa137bdaea22f1
KZ <kjreem.imagine@gmail.com>
2023-03-26 23:00:41 +0430

    [feat]auth: created register component

--- src/app/auth/components/register/register.component.spec.ts
+++ src/app/auth/components/register/register.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,9 +10,10 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [AuthenticationService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
@@ -19,5 +22,28 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should validate email', () => {
+    component.registeration_form.get('email')?.setValue('InvalidEmail');
+
+    expect(component.registeration_form.get('email')?.valid).toEqual(false);
+  });
+
+  it('should validate password', () => {
+    component.registeration_form.get('password')?.setValue('123');
+
+    expect(component.registeration_form.get('password')?.valid).toEqual(false);
+  });
+
+  it('should validate password confirmation', () => {
+    component.registeration_form.get('password')?.setValue('ValidPassword1');
+    component.registeration_form
+      .get('confirm_password')
+      ?.setValue('InvalidConfirmation');
+
+    expect(component.registeration_form.get('confirm_password')?.valid).toEqual(
+      false
+    );
   });
 });
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PasswordConfirmation } from '../../utils/validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registeration_form = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+    confirm_password: new FormControl('', [Validators.required,PasswordConfirmation()]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
