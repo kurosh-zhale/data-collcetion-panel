dea603cc9df477ff48e85b09fbcb07d31cb8b085
KZ <kjreem.imagine@gmail.com>
2023-03-26 22:58:53 +0430

    [feat]auth: created login component

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public login_form = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  constructor() { }
+  form_is_invalid = false;
 
-  ngOnInit(): void {
+  constructor(
+    private authServ: AuthenticationService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  private addValidators() {
+    this.login_form.get('username')?.addValidators([Validators.required]);
+    this.login_form.get('password')?.addValidators([Validators.required]);
+    this.login_form.get('username')?.updateValueAndValidity();
+    this.login_form.get('password')?.updateValueAndValidity();
   }
 
+  public submit() {
+    this.addValidators();
+    if (this.login_form.get('username')?.valid) {
+      this.login(this.login_form.value);
+    }
+  }
+
+  private login(info: any) {
+    this.authServ.PostLogin(info).subscribe((data)=>{
+      
+    })
+  }
 }
