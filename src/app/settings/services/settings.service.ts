76e951e7306bf9dbad009a5269a3abf271888792
KZ <kjreem.imagine@gmail.com>
2023-04-20 16:24:06 +0430

    [feat]settings: added new requests

--- src/app/settings/services/settings.service.ts
+++ src/app/settings/services/settings.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { SharedService } from 'src/app/shared/services/shared.service';
 import { environment } from 'src/environments/environment.prod';
 
@@ -7,10 +9,21 @@ import { environment } from 'src/environments/environment.prod';
   providedIn: 'root',
 })
 export class SettingsService {
-  constructor(private http: HttpClient, private sharedServ: SharedService) {}
+  constructor(
+    private http: HttpClient,
+    private sharedServ: SharedService,
+    private router: Router
+  ) {}
 
   get_organizations() {
     return this.sharedServ.get_organizations();
+  }
+
+  // will change after back-end update
+  get_members() {
+    return new Observable((suber) => {
+      suber.next(null);
+    });
   }
 
   create_organization(body: any) {
@@ -23,7 +36,13 @@ export class SettingsService {
     });
   }
 
-  update_user(user:any){
-    return this.http.patch(environment.baseUrl+'users/'+user._id,user);
+  update_user(user: any) {
+    return this.http.patch(environment.baseUrl + 'users/' + user._id, user);
+  }
+
+  navigate_to_Org(id: string) {
+    this.router.navigate(['settings', 'organization', id], {
+      skipLocationChange: false,
+    });
   }
 }
