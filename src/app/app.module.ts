4f631e4f53e4de172bcb5bba5d02dff1952b4933
KZ <kjreem.imagine@gmail.com>
2023-07-17 12:59:01 +0330

    [feat]src: added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment.prod';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  baseUrl:'https://api.molevision.de/'
+  baseUrl: 'https://api.molevision.de/',
+  firebase: {
+    apiKey: 'AIzaSyB-vkoRELEBC4mwPB1wh30a4o9m-VyfCGo',
+    authDomain: 'data-collection-angular.firebaseapp.com',
+    projectId: 'data-collection-angular',
+    storageBucket: 'data-collection-angular.appspot.com',
+    messagingSenderId: '435839016580',
+    appId: '1:435839016580:web:0679f6fcdf0e38e9492cd1',
+    measurementId: 'G-MVRRQKJEYW',
+  },
 };
