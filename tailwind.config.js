5fa99045d804de2e4fee4560bb1ad623330e231d
KZ <kjreem.imagine@gmail.com>
2023-07-21 14:15:03 +0330

    added tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    screens: {
+      mobile: { min: "320px", max: "425px" },
+      laptop: { min: "1344px", max: "1632px" },
+      desktop: { min: "1920px", max: "3264px" },
+    },
   },
   plugins: [],
 };
